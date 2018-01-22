684e53c5a5d1784bb22f84fd92387ad14cef448c
Michelle Youn <michelleyoun@michelles-air.bc.edu>
2018-01-22 10:36:10 -0500

    Initial Commit

---
+++ You Are Awesome/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  You Are Awesome
+//
+//  Created by Michelle Youn on 1/22/18.
+//  Copyright © 2018 Michelle Youn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
